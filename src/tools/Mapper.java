b9a7aa5d2cc75d2af99fc47b3c059a6ba262a60c
alexegiev <alexegievteo@gmail.com>
2024-04-05 14:52:15 +0300

    Added Mapper

---
+++ src/tools/Mapper.java
@@ -0,0 +1,31 @@
+package tools;
+/*
+* Purpose of mapper is to:
+*   Be called each time a client request is made
+*   Using a Hash function H(reqId) = reqId % numberOfWorkers
+*   Determine to which worker will the request be forward
+*/
+public class Mapper {
+
+    // Fields
+    private int numberOfWorkers;
+
+    // Constructor
+    public Mapper(int numberOfWorkers) {
+        this.numberOfWorkers = numberOfWorkers;
+    }
+
+    // Methods
+    public int mapRequest(int requestId) {
+        return requestId % numberOfWorkers;
+    }
+
+    // Getters and Setters
+    public int getNumberOfWorkers() {
+        return numberOfWorkers;
+    }
+
+    public void setNumberOfWorkers(int numberOfWorkers) {
+        this.numberOfWorkers = numberOfWorkers;
+    }
+}
