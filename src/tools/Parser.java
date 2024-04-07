648003e3b91252200c2ba603bef08e15ff3a514e
adrianakrg <adrikara2002@gmail.com>
2024-04-07 18:22:08 +0300

    changed the parsed,still doesnt work,comment it out if you work on something else

--- src/tools/Parser.java
+++ src/tools/Parser.java
@@ -1,41 +1,51 @@
-//package tools;
-//
-//import com.google.gson.Gson;
-//import com.google.gson.JsonArray;
-//import com.google.gson.JsonElement;
-//import com.google.gson.JsonParser;
-//import entities.Room;
-//import java.io.FileReader;
-//import java.io.IOException;
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//public class Parser {
-//
-//    public JsonArray readJsonFile(String filePath) {
-//        try (FileReader reader = new FileReader(filePath)) {
-//            return JsonParser.parseReader(reader).getAsJsonArray();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//            return null;
-//        }
-//    }
-//
-//    public List<Room> parseJsonArray(JsonArray jsonArray) {
-//        List<Room> rooms = new ArrayList<>();
-//        Gson gson = new Gson();
-//
-//        for (JsonElement jsonElement : jsonArray) {
-//            Room room = gson.fromJson(jsonElement, Room.class);
-//            rooms.add(room);
-//        }
-//
-//        return rooms;
-//    }
-//
-//    public void storeData(List<Room> rooms) {
-//        for (Room room : rooms) {
-//            System.out.println(room);
-//        }
-//    }
-//}
+package tools;
+
+import entities.Room;
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
+import javax.json.JsonValue;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+public class Parser {
+
+    public JsonArray readJsonFile(String filePath) {
+        try (JsonReader reader = Json.createReader(Files.newBufferedReader(Paths.get(filePath)))) {
+            return reader.readArray();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public List<Room> parseJsonArray(JsonArray jsonArray) {
+        List<Room> rooms = new ArrayList<>();
+
+        for (JsonValue jsonValue : jsonArray) {
+            JsonObject jsonObject = jsonValue.asJsonObject();
+            // Assuming Room has a constructor that accepts all necessary parameters
+            Room room = new Room(
+                    jsonObject.getString("roomName"),
+                    jsonObject.getInt("roomId"),
+                    jsonObject.getInt("managerId"),
+                    jsonObject.getString("area"),
+                    jsonObject.getJsonNumber("rating").doubleValue(),
+                    jsonObject.getInt("numberOfReviews"),
+                    jsonObject.getString("roomImage")
+            );
+            rooms.add(room);
+        }
+
+        return rooms;
+    }
+
+    public void storeData(List<Room> rooms) {
+        for (Room room : rooms) {
+            System.out.println(room);
+        }
+    }
+}
