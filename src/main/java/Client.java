1741ab21eba42d70837c2bc510c278604abfbfce
alexegiev <alexegievteo@gmail.com>
2024-04-11 14:38:09 +0300

    Working on Client. Actions for Manager need to be done

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -15,8 +15,8 @@ public class Client extends Thread{
     Request request;
 
     // List of Managers and Renters of the systems
-    static List<String> managers = Arrays.asList("Manager1", "Manager2", "Manager3");
-    static List<String> renters = Arrays.asList("Renter1", "Renter2", "Renter3");
+    static List<String> managers = Arrays.asList("manager1", "manager2", "manager3");
+    static List<String> renters = Arrays.asList("renter1", "renter2", "renter3");
 
     // Constructors
 
@@ -28,6 +28,7 @@ public class Client extends Thread{
     }
 
     private String login(){
+
         // ask Client if is Manager or Renter
         System.out.println("Are you a Manager or a Renter? (1: Manager, 2: Renter)");
 
@@ -40,10 +41,17 @@ public class Client extends Thread{
             choice = scanner.nextInt();
         }
 
-        String username;
-
-        // Create a new Client object
         if (choice == 1) {
+            return "Manager";
+        } else {
+            return "Renter";
+        }
+    }
+
+    private String validate(String role){
+        String username;
+        Scanner scanner = new Scanner(System.in);
+        if (role.equals("Manager")) {
             System.out.println("Enter Manager Username: ");
             username = scanner.next();
             while(true) {
@@ -54,7 +62,8 @@ public class Client extends Thread{
                 System.out.println("Invalid username. Please enter a valid username.");
                 username = scanner.next();
             }
-        } else {
+        }
+        else {
             System.out.println("Enter Renter Username: ");
             username = scanner.next();
             while(true) {
@@ -67,6 +76,71 @@ public class Client extends Thread{
             }
         }
         return username;
+    }
+
+    private static void managerActions(String username) {
+
+        // show available actions for manager
+        System.out.println("Choose available actions: " +
+                "\n1. Add Room" +
+                "\n2. Add Room Availability Date" +
+                "\n3. Show Owned Rooms" +
+                "\n4. Logout");
+
+        //TODO: Implement the actions
+        Scanner action = new Scanner(System.in);
+        int choice = action.nextInt();
+        while (choice != 4) {
+            switch (choice) {
+                case 1:
+                    // Add Room
+
+                    break;
+                case 2:
+                    // Add Room Availability Date
+                    break;
+                case 3:
+                    // Show Owned Rooms
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please enter a valid choice.");
+                    break;
+            }
+            choice = action.nextInt();
+        }
+        System.out.println("Logged out successfully.");
+    }
+
+    private static void renterActions(String username) {
+
+        // show available actions for renter
+        System.out.println("Choose available actions: " +
+                "\n1. Search Room" +
+                "\n2. Book Room" +
+                "\n3. Show Booked Rooms" +
+                "\n4. Logout");
+
+        //TODO: Implement the actions
+        Scanner action = new Scanner(System.in);
+        int choice = action.nextInt();
+        while (choice != 4) {
+            switch (choice) {
+                case 1:
+                    // Search Room
+                    break;
+                case 2:
+                    // Book Room
+                    break;
+                case 3:
+                    // Show Booked Rooms
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please enter a valid choice.");
+                    break;
+            }
+            choice = action.nextInt();
+        }
+        System.out.println("Logged out successfully.");
     }
 
     public void run() {
@@ -135,9 +209,16 @@ public class Client extends Thread{
 
         //Login
         Client client = new Client();
-        String username = client.login();
+        String role = client.login();
+        String username = client.validate(role);
 
-        //TODO: Show menu for Manager or Renter
+        // check client's role and call the appropriate method
+        if (role.equals("Manager")) {
+            managerActions(username);
+        } else {
+            renterActions(username);
+        }
+
         //TODO: Form the appropriate Request object
 
 
